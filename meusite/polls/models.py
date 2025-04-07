aa230df0ef96fe228d0b671f9445891879151846
João Pedro Silva Valentim <j.pedro__@hotmail.com>
2025-04-07 16:03:50 -0300

    modifiquei sem sentido nenhum por causa da 77
    foi isso

--- meusite/polls/models.py
+++ meusite/polls/models.py
@@ -14,3 +14,8 @@ class Choice(models.Model):
     choice_text =models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+#nao faz sentido nenhum
+class Choice02(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text =models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
